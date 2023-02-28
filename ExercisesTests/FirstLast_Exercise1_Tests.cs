477283daf9deb275c42ee5ad38bf794039c8fffc
krystyna.slusarczyk.learning@gmail.com <krystyna.slusarczyk.learning@gmail.com>
2023-02-28 18:24:47 +0100

    fixing the tests for the First and Last methods

--- ExercisesTests/FirstLast_Exercise1_Tests.cs
+++ ExercisesTests/FirstLast_Exercise1_Tests.cs
@@ -5,23 +5,21 @@ namespace ExercisesTests
     [TestFixture]
     public class FirstLast_Exercise1_Tests
     {
-        [TestFixture]
-        public class FirstTestSuite
+        [Test]
+        public void NameIsPresent()
         {
-            public void NameIsPresent()
-            {
-                var words = new[] { "abc", "K", "MArtin", "John", "Jack", "bbb" };
-                var result = FirstLast.FindFirstNameInTheCollection(words);
-                Assert.AreEqual("John", result, $"The first name in the collection (abc, K, MArtin, John, Jack, bbb) is John");
-            }
+            var words = new[] { "abc", "K", "MArtin", "John", "Jack", "bbb" };
+            var result = FirstLast.FindFirstNameInTheCollection(words);
+            Assert.AreEqual("John", result, $"The first name in the collection (abc, K, MArtin, John, Jack, bbb) is John");
+        }
 
-            [Test]
-            public void NameIsNotPresent()
-            {
-                var words = new[] { "abc", "K", "bbb" };
-                var result = FirstLast.FindFirstNameInTheCollection(words);
-                Assert.AreEqual(null, result, $"No word in the collection (abc, K, bbb) is a valid name");
-            }
+        [Test]
+        public void NameIsNotPresent()
+        {
+            var words = new[] { "abc", "K", "bbb" };
+            var result = FirstLast.FindFirstNameInTheCollection(words);
+            Assert.AreEqual(null, result, $"No word in the collection (abc, K, bbb) is a valid name");
         }
     }
+
 }
--- ExercisesTests/FirstLast_Exercise2_Tests.cs
+++ ExercisesTests/FirstLast_Exercise2_Tests.cs
@@ -9,32 +9,29 @@ namespace ExercisesTests
     [TestFixture]
     public class FirstLast_Exercise2_Tests
     {
-        [TestFixture]
-        public class FirstTestSuite
+        [Test]
+        public void ShallFindYoungest_InNonEmptyCollection()
         {
-            [Test]
-            public void ShallFindYoungest_InNonEmptyCollection()
-            {
-                var people = new List<Person>
+            var people = new List<Person>
             {
               new Person ("John", new DateTime(1986, 3, 4)),
               new Person ("Stephanie", new DateTime(1990, 12, 4)),
               new Person ("Mark", new DateTime(1957, 1, 2))
             };
-                var peopleAsString = string.Join(", ", people);
-                var result = FirstLast.GetYoungest(people);
-                Assert.AreEqual(people[1], result, $"The test failed, because the youngest person in ({peopleAsString}) collection is Stephanie, and the result is {result}");
-            }
+            var peopleAsString = string.Join(", ", people);
+            var result = FirstLast.GetYoungest(people);
+            Assert.AreEqual(people[1], result, $"The test failed, because the youngest person in ({peopleAsString}) collection is Stephanie, and the result is {result}");
+        }
 
-            [Test]
-            public void EmptyCollection()
+        [Test]
+        public void EmptyCollection()
+        {
+            var people = new List<Person>
             {
-                var people = new List<Person>
-                {
-                };
-                var result = FirstLast.GetYoungest(people);
-                Assert.AreEqual(null, result, "For empty collection the result should be null");
-            }
+            };
+            var result = FirstLast.GetYoungest(people);
+            Assert.AreEqual(null, result, "For empty collection the result should be null");
         }
     }
 }
+
